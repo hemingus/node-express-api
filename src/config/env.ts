2809c9243d65825ac55a36a207add6c57b74060e
Heming Hanevik <heming.hanevik@gmail.com>
2026-01-05 08:16:34 +0100

    update portnr

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,2 +1,2 @@
 export const JWT_SECRET = process.env.JWT_SECRET ?? "dev-secret";
-export const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+export const PORT = process.env.PORT ? Number(process.env.PORT) : 3500;
