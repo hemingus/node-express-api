70ee3b860d7d2ec90c1bc5c07a0a1bfc846a6d7e
Heming Hanevik <heming.hanevik@gmail.com>
2026-01-05 08:18:16 +0100

    add 4 hardcoded test users

--- src/data/users.store.ts
+++ src/data/users.store.ts
@@ -8,7 +8,22 @@ export interface User {
 export const users: User[] = [
     {
         id: "1",
-        username: "testuser",
-        password: "password123",
+        username: "alfa",
+        password: "alfa123",
+    },
+        {
+        id: "2",
+        username: "beta",
+        password: "beta123",
+    },
+        {
+        id: "3",
+        username: "gamma",
+        password: "gamma123",
+    },
+        {
+        id: "4",
+        username: "delta",
+        password: "delta123",
     },
 ];
